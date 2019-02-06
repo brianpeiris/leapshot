ea6ec8e354152ac1e8a876308f7c57c0c78d701f
Brian Peiris <brian@peiris.io>
2019-02-06 11:48:50 -0800

    Add nodemon and browser-sync to dev dependencies

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-nodemon -w src/server/index.js src/server/index.js &
-browser-sync start -s -w -f src/client/ --no-open --no-notify --no-ghost-ui --directory --ss ~/Pictures/Screenshots/ &
-node_modules/.bin/webpack --entry=$PWD/src/client/app.js --output dist/app.js --mode development --watch &
-node_modules/.bin/webpack --entry=$PWD/src/client/cursor.js --output dist/cursor.js --mode development --watch &
+npx nodemon -w src/server/index.js src/server/index.js &
+npx browser-sync start -s -w -f src/client/ --no-open --no-notify --no-ghost-ui --directory --ss ~/Pictures/Screenshots/ &
+node_modules/.bin/webpack --entry="$PWD/src/client/app.js" --output dist/app.js --mode development --watch &
+node_modules/.bin/webpack --entry="$PWD/src/client/cursor.js" --output dist/cursor.js --mode development --watch &
 trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
 read
